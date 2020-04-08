6fb47b69a6a449c6b6525fe0599d7341ebd8dc7b
sundar_z <sundar.zsd@gmail.com>
2020-04-08 21:56:32 +0800

    ,

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < ApplicationController
   end
 
   def update
-    permit_params = params.require(:setting).permit(:title, :description, :domain_name,  :platform, :region, :account, :password, :platform_id, :wait_time, :shielding,  :load_type, :auxiliary_platform, :auxiliary_time, :vpn_setting, :collection_type,  :tool_type, :facebook_account, :facebook_password, :facebook_setting :nickname_type, :custom_nickname, :friend_accouts,  :friend_count, password_setting: [], function: [], custom_function: [], input_setting: [], add_type: [])
+    permit_params = params.require(:setting).permit(:title, :description, :domain_name,  :platform, :region, :account, :password, :platform_id, :wait_time, :shielding,  :load_type, :auxiliary_platform, :auxiliary_time, :vpn_setting, :collection_type,  :tool_type, :facebook_account, :facebook_password, :facebook_setting, :nickname_type, :custom_nickname, :friend_accouts,  :friend_count, password_setting: [], function: [], custom_function: [], input_setting: [], add_type: [])
 
     if @setting.update(permit_params)
       redirect_to root_path, notice: "修改成功！"
